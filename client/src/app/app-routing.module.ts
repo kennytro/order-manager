6b2a0bf1ecec42f0f47d071090d858ff3cc9b168
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-01-23 00:02:46 -0800

    Add main components.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ClientComponent } from './components/client/client.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProcessComponent } from './components/process/process.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'client',
+    component: ClientComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'process',
+    component: ProcessComponent
+  },
+  {
+    path: 'contactus',
+    component: ContactUsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SDKBrowserModule } from './shared/sdk/index';
+import { HomeComponent } from './components/home/home.component';
+import { ClientComponent } from './components/client/client.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProcessComponent } from './components/process/process.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientComponent,
+    ProductComponent,
+    ProcessComponent,
+    LoginComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
