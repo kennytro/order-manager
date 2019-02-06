832896c48d2d6fac9f0cf073aed4cbe03f70aac4
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-02-05 21:08:47 -0800

    Upgrade to angular-fontawesome.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { SDKBrowserModule } from './shared/sdk/index';
 import { PublicModule } from './modules/public/public.module';
+
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 import { AuthService } from './services/auth.service';
 import { AuthGuardService } from './services/auth-guard.service';
 import { RootScopeShareService } from './services/root-scope-share.service';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
--- client/src/app/modules/public/public.module.ts
+++ client/src/app/modules/public/public.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +10,9 @@ import { ClientComponent } from './components/client/client.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProcessComponent } from './components/process/process.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMobile, faSignInAlt, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,12 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     PublicRoutingModule
   ]
 })
-export class PublicModule { }
+export class PublicModule {
+  constructor() {
+    library.add(faMobile, faSignInAlt, faInfoCircle);
+  }
+}
