b5fe9d2e104e5286820bb51da13d6186790d38a0
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-17 21:04:46 -0700

    Make 'unit price' column of 'Products' table editable.

--- client/src/app/modules/auth/modules/employee/components/products/products/products.component.ts
+++ client/src/app/modules/auth/modules/employee/components/products/products/products.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import { MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import { NewProductComponent } from '../new-product/new-product.component';
 import { DataApiService } from '../../../services/data-api.service';
@@ -15,6 +18,7 @@ export class ProductsComponent implements OnInit {
   // TO DO: get column names from service.
   displayedColumns: string[] = ['id', 'name', 'description', 'category', 'originCountry', 'unitPrice'];
   private _products: MatTableDataSource<any>;
+  private _unsubscribe = new Subject<boolean>();  
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -29,6 +33,11 @@ export class ProductsComponent implements OnInit {
         this._setTableDataSource(routeData['products']);
       }
     });
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe.next(true);
+    this._unsubscribe.unsubscribe();
   }
 
   applyFilter(filterValue: string) {
@@ -62,8 +71,27 @@ export class ProductsComponent implements OnInit {
   }
 
   private _setTableDataSource(products: Array<any>) {
+    products.forEach((product) => {
+      product.unitPriceFC = new FormControl(product.unitPrice);
+      product.unitPriceFC.valueChanges
+        .pipe(takeUntil(this._unsubscribe), debounceTime(1000), distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)))
+        .subscribe(value => {
+          product.unitPrice = value;
+          this._updateUnitPrice(product);
+        })
+    });
     this._products = new MatTableDataSource(products);
     this._products.paginator = this.paginator;
     this._products.sort = this.sort;   
+  }
+
+  private async _updateUnitPrice(product: any) {
+    try {
+      // updateAll() is better than 'upsertWithWhere' since it bypasses operation hooks of 'Product'
+      await this._dataApi.genericMethod('Product', 'updateAll', [{ id: product.id }, {unitPrice: product.unitPrice}]).toPromise();
+      console.log(`successfully saved price(id: ${product.id}, new price: ${product.unitPrice})`);
+    } catch (err) {
+      console.log(`error: failed to update price(id: ${product.id}, new price: ${product.unitPrice}) - ${err.message}`);
+    }
   }
 }
