6e5cd9d1247fcf67670c6c36b3da81912dd2f78d
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-20 13:37:43 -0700

    Update unit test spec for LocalScopeShareService.

--- client/src/app/modules/public/services/local-scope-share.service.spec.ts
+++ client/src/app/modules/public/services/local-scope-share.service.spec.ts
@@ -3,10 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { LocalScopeShareService } from './local-scope-share.service';
 
 describe('LocalScopeShareService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [LocalScopeShareService]
+  }));
 
   it('should be created', () => {
     const service: LocalScopeShareService = TestBed.get(LocalScopeShareService);
     expect(service).toBeTruthy();
+  });
+  it('should set data', () => {
+    const service: LocalScopeShareService = TestBed.get(LocalScopeShareService);
+    service.setData('testKey', 'testVal');
+    expect(service.getData('testKey')).toEqual('testVal', 'expected getData() to return testVal');
+  });
+  it('should remove data', () => {
+    const service: LocalScopeShareService = TestBed.get(LocalScopeShareService);
+    service.setData('testKey', 'testVal');
+    service.removeData('testKey');
+    expect(service.getData('testKey')).toEqual(undefined, 'expected getData() to return undefined');
   });
 });
