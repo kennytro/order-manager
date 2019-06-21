f306f5f434f65c486ae133791be5f332ac88cf1c
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-20 21:14:10 -0700

    Update unit test spec for AlertService.

--- client/src/app/modules/auth/shared/services/alert.service.spec.ts
+++ client/src/app/modules/auth/shared/services/alert.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import swal from 'sweetalert2';
 import { AlertService } from './alert.service';
 
 describe('AlertService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ AlertService ]
+  }));
 
   it('should be created', () => {
     const service: AlertService = TestBed.get(AlertService);
     expect(service).toBeTruthy();
   });
+  // TO DO: test alertSuccess() & confirm()
 });
