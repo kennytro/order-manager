ef195be0234e6bb00103f8b4a920b70b942c41e8
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-20 22:38:36 -0700

    Update unit test spec for ConfirmDialogComponent.

--- client/src/app/modules/auth/shared/components/confirm-dialog/confirm-dialog.component.spec.ts
+++ client/src/app/modules/auth/shared/components/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,25 +1,59 @@
+import { Component, NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MatDialog, MatDialogModule } from '@angular/material';
+import { AuthSharedModule } from '../../auth-shared.module';
+import { ConfirmDialogComponent, DialogData } from './confirm-dialog.component';
 
-import { ConfirmDialogComponent } from './confirm-dialog.component';
+// Noop component is only a workaround to trigger change detection
+@Component({
+  template: ''
+})
+class NoopComponent {}
+
+const TEST_DIRECTIVES = [
+  NoopComponent
+];
+
+@NgModule({
+  imports: [MatDialogModule, NoopAnimationsModule, AuthSharedModule],
+  exports: TEST_DIRECTIVES,
+  declarations: TEST_DIRECTIVES,
+  entryComponents: [
+    ConfirmDialogComponent
+  ],
+})
+class DialogTestModule { }
+
 
 describe('ConfirmDialogComponent', () => {
-  let component: ConfirmDialogComponent;
-  let fixture: ComponentFixture<ConfirmDialogComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ConfirmDialogComponent ]
-    })
-    .compileComponents();
-  }));
+  let dialog: MatDialog;
+  let overlayContainerElement: HTMLElement;
+  let noop: ComponentFixture<NoopComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ConfirmDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [ DialogTestModule ],
+      providers: [
+        { provide: OverlayContainer, useFactory: () => {
+          overlayContainerElement = document.createElement('div');
+          return { getContainerElement: () => overlayContainerElement };
+        }}
+      ]
+    });
+
+    dialog = TestBed.get(MatDialog);
+    noop = TestBed.createComponent(NoopComponent);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const dialogData: DialogData = {
+      title: 'Are you sure?'
+    };
+    dialog.open(ConfirmDialogComponent, { data: dialogData });
+    noop.detectChanges(); // Updates the dialog in the overlay
+    const h2 = overlayContainerElement.querySelector('#mat-dialog-title-0');
+    expect(h2.textContent).toBe('Are you sure?');
   });
 });
