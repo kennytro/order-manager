b5de6f4b5b779539104532691f0a83036008acb7
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-21 17:45:13 -0700

    Update unit test spec for UsersComponent.

--- client/src/app/modules/auth/modules/employee/components/users/users/users.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/users/users/users.component.spec.ts
@@ -1,25 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
+import { EmployeeModule } from '../../../employee.module';
 import { UsersComponent } from './users.component';
+import { AuthService } from '../../../../../../../services/auth.service';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
-
+  let authSvcSpy: jasmine.SpyObj<AuthService>;
+  let apiSpy: jasmine.SpyObj<DataApiService>;
   beforeEach(async(() => {
+    const testUsers = [
+      { id: 'kenny', email: 'kenny@etr.com', role: 'customer', clientId: '111', createDate: '01/01/2019'}
+    ];
+    const route = ({ data: of({ users: testUsers} ), snapshot: {} } as any) as ActivatedRoute;    
+    const authSpy = jasmine.createSpyObj('AuthService', ['isDemoUser']);
+    const dataSpy = jasmine.createSpyObj('DataApiService', ['find']); 
     TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      imports: [ NoopAnimationsModule, EmployeeModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        MatDialog,
+        MatSnackBar,
+        { provide: AuthService, useValue: authSpy },
+        { provide: DataApiService, useValue: dataSpy }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UsersComponent);
+    authSvcSpy = TestBed.get(AuthService);
+    apiSpy = TestBed.get(DataApiService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', (done) => {
     expect(component).toBeTruthy();
-  });
+    done();
+  }, 20000);
+  it('should count users', (done) => {
+    expect(component.getUsersCount()).toEqual(1, 'returned length of testUsers.');
+    done();
+  }, 20000);
 });
