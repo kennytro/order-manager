a273d80039d9f60f1563dd41667eb0c21e2c8a01
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-24 11:43:59 -0700

    Update unit test spec of NewUserComponent.

--- client/src/app/modules/auth/modules/employee/components/users/new-user/new-user.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/users/new-user/new-user.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { NewUserComponent } from './new-user.component';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('NewUserComponent', () => {
   let component: NewUserComponent;
   let fixture: ComponentFixture<NewUserComponent>;
-
+  let snackbarSpy: jasmine.SpyObj<MatSnackBar>;
+  let apiSpy: jasmine.SpyObj<DataApiService>;
   beforeEach(async(() => {
+    const dlgSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+    const msbSpy = jasmine.createSpyObj('MatSnackBar', ['open']);
+    const dataSpy = jasmine.createSpyObj('DataApiService', ['find', 'upsert']);
     TestBed.configureTestingModule({
-      declarations: [ NewUserComponent ]
+      declarations: [ NewUserComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: MatDialogRef, useValue: dlgSpy },
+        FormBuilder,
+        { provide: MatSnackBar, useValue: msbSpy },
+        { provide: DataApiService, useValue: dataSpy },
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +34,9 @@ describe('NewUserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NewUserComponent);
     component = fixture.componentInstance;
+    snackbarSpy = TestBed.get(MatSnackBar);
+    apiSpy = TestBed.get(DataApiService);
+    apiSpy.find.and.returnValue(of([{id: 'test1', name: 'testClient'}]));
     fixture.detectChanges();
   });
 
