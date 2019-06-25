f0db2596374401595bc3dd5d7e7c9cdc20fdfbd3
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-25 16:06:05 -0700

    Update unit test spec of ClosedOrders component.

--- client/src/app/modules/auth/modules/employee/components/orders/closed-orders/closed-orders.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/orders/closed-orders/closed-orders.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { ClosedOrdersComponent } from './closed-orders.component';
+import { FileService } from '../../../../../shared/services/file.service';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('ClosedOrdersComponent', () => {
+  const testOrders = [
+    { id: '1000', client: { id: '1100', name: 'test' }, status: 'Completed', totalAmount: '100.01', note: '', createdAt: '1/1/2019' },
+  ];
   let component: ClosedOrdersComponent;
   let fixture: ComponentFixture<ClosedOrdersComponent>;
 
   beforeEach(async(() => {
+    const route = ({ data: of({ orders: testOrders} ), snapshot: {} } as any) as ActivatedRoute;
     TestBed.configureTestingModule({
-      declarations: [ ClosedOrdersComponent ]
+      declarations: [ ClosedOrdersComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: FileService, useValue: jasmine.createSpyObj('FileService', ['downloadPDF']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['genericMethod']) }
+      ]
     })
     .compileComponents();
   }));
