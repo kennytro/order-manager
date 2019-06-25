b13c0dc84bffe45f3f90424547c8fb648f79412f
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-24 18:24:42 -0700

    Update unit test spec of Statements components.

--- client/src/app/modules/auth/modules/customer/components/statements/statements/statements.component.spec.ts
+++ client/src/app/modules/auth/modules/customer/components/statements/statements/statements.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 
 import { StatementsComponent } from './statements.component';
+
+import { FileService } from '../../../../../shared/services/file.service';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('StatementsComponent', () => {
   let component: StatementsComponent;
   let fixture: ComponentFixture<StatementsComponent>;
 
   beforeEach(async(() => {
+    const testStatements = [
+      { id: '11100', statementDate: '1/1/2019', totalAmount: '100.00', note: ''}
+    ];
+    const route = ({ data: of({ statements: testStatements} ), snapshot: {} } as any) as ActivatedRoute;
+    const dataSpy = jasmine.createSpyObj('DataApiService', ['genericMethod']);
+    const fsSpy = jasmine.createSpyObj('FileService', ['downloadPDF']);
+
     TestBed.configureTestingModule({
-      declarations: [ StatementsComponent ]
+      declarations: [ StatementsComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: FileService, useValue: fsSpy },
+        { provide: DataApiService, useValue: dataSpy }
+      ]
     })
     .compileComponents();
   }));
--- client/src/app/modules/auth/modules/employee/components/statements/statements/statements.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/statements/statements/statements.component.spec.ts
@@ -1,14 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus, faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import { of } from 'rxjs';
+
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 
 import { StatementsComponent } from './statements.component';
+
+import { FileService } from '../../../../../shared/services/file.service';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('StatementsComponent', () => {
   let component: StatementsComponent;
   let fixture: ComponentFixture<StatementsComponent>;
+  let apiSpy: jasmine.SpyObj<DataApiService>;
+  library.add(faPlus, faFilePdf);
 
   beforeEach(async(() => {
+    const testStatements = [
+      { id: '11100', client: {id: '111', name: 'testClient'}, statementDate: '1/1/2019', totalAmount: '100.00', note: ''}
+    ];
+    const route = ({ data: of({ statements: testStatements} ), snapshot: {} } as any) as ActivatedRoute;
+    const dataSpy = jasmine.createSpyObj('DataApiService', ['genericMethod']);
+    const fsSpy = jasmine.createSpyObj('FileService', ['downloadPDF']);
+
     TestBed.configureTestingModule({
-      declarations: [ StatementsComponent ]
+      declarations: [ StatementsComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: FileService, useValue: fsSpy },
+        { provide: DataApiService, useValue: dataSpy }
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +42,7 @@ describe('StatementsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StatementsComponent);
     component = fixture.componentInstance;
+    apiSpy = TestBed.get(DataApiService);
     fixture.detectChanges();
   });
 
