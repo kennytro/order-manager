6c415322cd95365c01beabec7977d69205103020
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-24 20:57:07 -0700

    Update unit test spec of NewStatement component.

--- client/src/app/modules/auth/modules/employee/components/statements/new-statement/new-statement.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/statements/new-statement/new-statement.component.spec.ts
@@ -1,14 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLongArrowAltRight, faStoreAlt, faShoppingCart, faSearchDollar } from '@fortawesome/free-solid-svg-icons';
 
+import { of } from 'rxjs';
+
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { NewStatementComponent } from './new-statement.component';
-
+import { DataApiService } from '../../../services/data-api.service';
 describe('NewStatementComponent', () => {
   let component: NewStatementComponent;
   let fixture: ComponentFixture<NewStatementComponent>;
+  library.add(faLongArrowAltRight, faStoreAlt, faShoppingCart, faSearchDollar);
 
   beforeEach(async(() => {
+    const route = ({ data: of({ clients: [{id: '111', name: 'testClient' }] }), snapshot: {} } as any) as ActivatedRoute;
+
     TestBed.configureTestingModule({
-      declarations: [ NewStatementComponent ]
+      declarations: [ NewStatementComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        FormBuilder,
+        { provide: MatDialog, useValue: { open: () => {} } },
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['genericMethod']) }
+      ]
     })
     .compileComponents();
   }));
