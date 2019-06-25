54fb49aad2f0a1ff3c134fa166bfbe0403268c94
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-24 21:45:33 -0700

    Update unit test spec of NewProduct component.

--- client/src/app/modules/auth/modules/employee/components/products/new-product/new-product.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/products/new-product/new-product.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+import { FormBuilder } from '@angular/forms';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { NewProductComponent } from './new-product.component';
+import { DataApiService } from '../../../services/data-api.service';
+import { ProductService } from '../../../services/product.service';
 
 describe('NewProductComponent', () => {
   let component: NewProductComponent;
@@ -8,7 +16,15 @@ describe('NewProductComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewProductComponent ]
+      declarations: [ NewProductComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule, ImageCropperModule],
+      providers: [
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) },
+        FormBuilder,
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['upsert']) },
+        { provide: ProductService, useValue: jasmine.createSpyObj('ProductService', ['getNoPictureImage', 'putImageToS3']) }
+      ]
     })
     .compileComponents();
   }));
