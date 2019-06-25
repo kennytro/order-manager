615af25ee7497cb264674e0be328ec19bf87b5fe
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-24 21:21:56 -0700

    Update unit test spec of Products component.

--- client/src/app/modules/auth/modules/employee/components/products/products/products.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/products/products/products.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { ProductsComponent } from './products.component';
-
+import { DataApiService } from '../../../services/data-api.service';
 describe('ProductsComponent', () => {
+  const testProducts = [
+    {
+      id: '1111', name: 'grape', description: 'Grape', category: 'fruit', originCountry: 'USA', unitPrice: '10.00', unit: 'lb', settings: {}
+    }
+  ];
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async(() => {
+    const route = ({ data: of({ products: testProducts} ), snapshot: {} } as any) as ActivatedRoute;
+
     TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ ProductsComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: MatDialog, useValue: jasmine.createSpyObj('MatDialog', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['find', 'genericMethod']) }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +40,8 @@ describe('ProductsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should count product', () => {
+    expect(component.getProductsCount()).toEqual(testProducts.length);
   });
 });
