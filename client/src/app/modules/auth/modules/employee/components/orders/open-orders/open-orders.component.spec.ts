a8583fe7c19fc41b65cfb4c06a07834ea0dd4a0d
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-25 15:45:42 -0700

    Update unit test spec of OpenOrders component.

--- client/src/app/modules/auth/modules/employee/components/orders/open-orders/open-orders.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/orders/open-orders/open-orders.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { of } from 'rxjs';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { OpenOrdersComponent } from './open-orders.component';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('OpenOrdersComponent', () => {
+  const testOrders = [
+    { id: '1000', client: { id: '1100', name: 'test' }, status: 'Submitted', totalAmount: '100.01', note: '', createdAt: '1/1/2019' },
+    { id: '1001', client: { id: '1101', name: 'test1' }, status: 'Processed', totalAmount: '100.01', note: '', createdAt: '1/1/2019' },
+    { id: '1002', client: { id: '1102', name: 'test2' }, status: 'Shipped', totalAmount: '100.01', note: '', createdAt: '1/1/2019' }
+  ];
   let component: OpenOrdersComponent;
   let fixture: ComponentFixture<OpenOrdersComponent>;
 
   beforeEach(async(() => {
+    const route = ({ data: of({ orders: testOrders} ), snapshot: {} } as any) as ActivatedRoute;
     TestBed.configureTestingModule({
-      declarations: [ OpenOrdersComponent ]
+      declarations: [ OpenOrdersComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: MatDialog, useValue: jasmine.createSpyObj('MatDialog', ['open']) },
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['genericMethod', 'find']) }
+      ]
     })
     .compileComponents();
   }));
