fea6d843ad8d29eaeba1fb3280e19e678a790ea2
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-25 13:36:32 -0700

    Update unit test spec of OrderLayout component.

--- client/src/app/modules/auth/modules/employee/components/orders/order-layout/order-layout.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/orders/order-layout/order-layout.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { OrderLayoutComponent } from './order-layout.component';
 
 describe('OrderLayoutComponent', () => {
   let component: OrderLayoutComponent;
   let fixture: ComponentFixture<OrderLayoutComponent>;
+  library.add(faCartPlus);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderLayoutComponent ]
+      declarations: [ OrderLayoutComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule]
     })
     .compileComponents();
   }));
