1a8ed0a125a8b8fa31f600d33365e8fb8c6de716
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-25 14:54:39 -0700

    Update unit test spec of OrderDetail component.

--- client/src/app/modules/auth/modules/employee/components/orders/order-detail/order-detail.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/orders/order-detail/order-detail.component.spec.ts
@@ -1,14 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Location } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { FormBuilder } from '@angular/forms';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { of } from 'rxjs';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { OrderDetailComponent } from './order-detail.component';
+import { FileService } from '../../../../../shared/services/file.service';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('OrderDetailComponent', () => {
+  const testOrder = {
+    id: '1000',
+    client: { id: '2000', name: 'testClient' },
+    status: 'Submitted',
+    subtotal: '100.00',
+    fee: '5.00',
+    totalAmount: '105.00',
+    orderItem: [],
+    userCreated: { id: '1000', email: 'kenny@etr.com' },
+    userUpdated: { id: '1000', email: 'kenny2@etr.com' },
+  }
   let component: OrderDetailComponent;
   let fixture: ComponentFixture<OrderDetailComponent>;
+  library.add(faQuestionCircle);
 
   beforeEach(async(() => {
+    const route = ({ data: of({ orderInfo: { order: testOrder, products: [] } } ), snapshot: {} } as any) as ActivatedRoute;
+
     TestBed.configureTestingModule({
-      declarations: [ OrderDetailComponent ]
+      declarations: [ OrderDetailComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        FormBuilder,
+        { provide: Location, useValue: jasmine.createSpyObj('Location', ['back', 'path']) },
+        { provide: MatDialog, useValue: jasmine.createSpyObj('MatDialog', ['open']) },
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
+        { provide: FileService, useValue: jasmine.createSpyObj('FileService', ['downloadPDF']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['genericMethod']) }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +57,9 @@ describe('OrderDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should get email', () => {
+    expect(component.getEmailCreated()).toEqual(testOrder.userCreated.email, 'expected creator email');
+    expect(component.getEmailUpdated()).toEqual(testOrder.userUpdated.email, 'expected updator email');
   });
 });
