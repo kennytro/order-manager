7ca0e06c61c3d05182859cba60937354d00875c0
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-26 15:21:53 -0700

    Update unit test spec of TodaysSnapshots component.

--- client/src/app/modules/auth/modules/employee/components/dashboard/todays-snapshots/todays-snapshots.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/dashboard/todays-snapshots/todays-snapshots.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 import * as moment from 'moment';
