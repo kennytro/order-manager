9b51bf4e0f84187b2176028892b6ac8cc999768f
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-26 11:48:14 -0700

    Update unit test spec of Clients component.

--- client/src/app/modules/auth/modules/employee/components/clients/clients/clients.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/clients/clients/clients.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { ClientsComponent } from './clients.component';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('ClientsComponent', () => {
+  const testClients = [
+    { id: '1000', name: 'testA', phone: '1111111111', deliveryRouteId: 'routeA', createdDate: '1/1/2019' }
+  ];
   let component: ClientsComponent;
   let fixture: ComponentFixture<ClientsComponent>;
 
   beforeEach(async(() => {
+    const route = ({ data: of({ clients: testClients} ), snapshot: {} } as any) as ActivatedRoute;
     TestBed.configureTestingModule({
-      declarations: [ ClientsComponent ]
+      declarations: [ ClientsComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule ],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: MatDialog, useValue: jasmine.createSpyObj('MatDialog', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['find']) }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +39,7 @@ describe('ClientsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should count clients', () => {
+    expect(component.getClientsCount()).toEqual(testClients.length, 'expect test clients count');
+  });  
 });
