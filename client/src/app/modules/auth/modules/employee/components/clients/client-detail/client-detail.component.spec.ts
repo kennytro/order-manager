12493e09f0ec0a52aa4a9dee9415e05055848af1
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-26 12:00:16 -0700

    Update unit test spec of ClientDetail component.

--- client/src/app/modules/auth/modules/employee/components/clients/client-detail/client-detail.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/clients/client-detail/client-detail.component.spec.ts
@@ -1,14 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
+import { of } from 'rxjs';
 
+import { TextMaskModule } from 'angular2-text-mask';
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { ClientDetailComponent } from './client-detail.component';
+import { AlertService } from '../../../../../shared/services/alert.service';
+import { DataApiService } from '../../../services/data-api.service';
 
 describe('ClientDetailComponent', () => {
+  const testClient = { id: '1000', name: 'testA', phone: '1111111111', deliveryRouteId: 'routeA', createdDate: '1/1/2019' };
+  const testRoutes = [
+    { id: '1000', description: 'test Route', driverName: 'Test Driver', driverPhone: '1111111111' }
+  ];
   let component: ClientDetailComponent;
   let fixture: ComponentFixture<ClientDetailComponent>;
 
   beforeEach(async(() => {
+    const route = ({ data: of({ client: testClient, deliveryRoutes: testRoutes} ), snapshot: {} } as any) as ActivatedRoute;
     TestBed.configureTestingModule({
-      declarations: [ ClientDetailComponent ]
+      declarations: [ ClientDetailComponent ],
+      imports: [ NoopAnimationsModule, RouterTestingModule, AuthSharedModule, TextMaskModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+        { provide: AlertService, useValue: jasmine.createSpyObj('AlertService', ['confirm', 'alertSuccess']) },
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['destroyById', 'upsert']) }
+      ]
     })
     .compileComponents();
   }));
