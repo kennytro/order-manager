a62c26ff8e5856224240bac395bc643608927fc2
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-25 21:46:51 -0700

    Update unit test spec of NewDeliveryRoute component.

--- client/src/app/modules/auth/modules/employee/components/delivery-routes/new-delivery-route/new-delivery-route.component.spec.ts
+++ client/src/app/modules/auth/modules/employee/components/delivery-routes/new-delivery-route/new-delivery-route.component.spec.ts
@@ -1,14 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+import { FormBuilder } from '@angular/forms';
 
+import { TextMaskModule } from 'angular2-text-mask';
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { NewDeliveryRouteComponent } from './new-delivery-route.component';
-
+import { DataApiService } from '../../../services/data-api.service';
 describe('NewDeliveryRouteComponent', () => {
   let component: NewDeliveryRouteComponent;
   let fixture: ComponentFixture<NewDeliveryRouteComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewDeliveryRouteComponent ]
+      declarations: [ NewDeliveryRouteComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule, RouterTestingModule, TextMaskModule],
+      providers: [
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) },
+        FormBuilder,
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
+        { provide: DataApiService, useValue: jasmine.createSpyObj('DataApiService', ['genericMethod']) }
+      ]
     })
     .compileComponents();
   }));
