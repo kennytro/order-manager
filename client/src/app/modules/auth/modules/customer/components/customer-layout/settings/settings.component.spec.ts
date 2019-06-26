461b9f31c954e8b790a93a98a9d77cad05af9e0b
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-06-26 15:28:33 -0700

    Update unit test spec of Settings componet in customer module.

--- client/src/app/modules/auth/modules/customer/components/customer-layout/settings/settings.component.spec.ts
+++ client/src/app/modules/auth/modules/customer/components/customer-layout/settings/settings.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef } from '@angular/material';
 
+import { AuthSharedModule } from '../../../../../shared/auth-shared.module';
 import { SettingsComponent } from './settings.component';
 
 describe('SettingsComponent', () => {
@@ -8,7 +11,11 @@ describe('SettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [ SettingsComponent ],
+      imports: [ NoopAnimationsModule, AuthSharedModule ],
+      providers: [
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) },
+      ]
     })
     .compileComponents();
   }));
