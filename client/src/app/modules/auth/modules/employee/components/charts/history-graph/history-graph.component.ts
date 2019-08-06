0966e9b5fca90cee0455012e98927f2dc423303a
RO_DESKTOP\Kwang <k3nnyro@yahoo.com>
2019-08-05 17:06:22 -0700

    Add day range selection to history graph component.

--- client/src/app/modules/auth/modules/employee/components/charts/history-graph/history-graph.component.ts
+++ client/src/app/modules/auth/modules/employee/components/charts/history-graph/history-graph.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
-import { take } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 import * as moment from 'moment';
 
 import { DataApiService } from '../../../services/data-api.service';
@@ -11,14 +13,25 @@ import { DataApiService } from '../../../services/data-api.service';
   styleUrls: ['./history-graph.component.css']
 })
 export class HistoryGraphComponent implements OnInit {
+  ranges = [
+    { label: '30 Days', cutOffDate: moment().subtract(30, 'days').toDate() },
+    { label: '90 Days', cutOffDate: moment().subtract(90, 'days').toDate() },
+    { label: '1 Year', cutOffDate: moment().subtract(1, 'years').toDate() },
+    { label: '3 Year', cutOffDate: moment().subtract(1, 'years').toDate() }
+  ];
+  rangeSelection: FormControl;
+
   historyChart: GoogleChartInterface;
 //  showSpinner = false;
+  private _unsubscribe = new Subject<boolean>();
+
+  private _cutOffDate: Date;
   private _instanceId: string;
   @Input()
   set instanceId(id: string) {
     if (this._instanceId !== id) {
       this._instanceId = id;
-      this._updateChart(id);
+      this._updateChart();
     }
   }
   get instanceId(): string { return this._instanceId; }
@@ -29,53 +42,110 @@ export class HistoryGraphComponent implements OnInit {
 
   @Input() chartType: string = 'LineChart';
 
-  constructor(private _dataApi: DataApiService) { }
+  constructor(private _dataApi: DataApiService) {
+    this._cutOffDate = this.ranges[0].cutOffDate;
+  }
 
   ngOnInit() {
+    this.rangeSelection = new FormControl(this.ranges[0].label);
+    this.rangeSelection.valueChanges
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(rangeLabel => {
+        let rangeInfo = this.ranges.find(element => element.label === rangeLabel);
+        if (this._cutOffDate !== rangeInfo.cutOffDate) {
+          this._cutOffDate = rangeInfo.cutOffDate;
+          this._updateChart();
+        }
+      });
+
     this._dataApi.genericMethod('Metric', 'findOne', [{ where: { name: this.metricName } }])
       .pipe(take(1))
       .subscribe(metric => {
         if (metric) {
           this._metricDefinition = metric;
-          this._initializeChart(metric);
         }
       });
   }
 
-  private _updateChart(instanceId: string) {
-    if (instanceId) {
+  ngOnDestroy() {
+    this._unsubscribe.next(true);
+    this._unsubscribe.unsubscribe();
+  }
+
+  private _updateChart() {
+    if (this._instanceId) {
 //      this.showSpinner = true;
-      this._dataApi.genericMethod('Metric', 'findMetricDataByName', [[this._metricDefinition.name],  { instanceId: instanceId }])
+      this._dataApi.genericMethod('Metric', 'findMetricDataByName',
+        [[this._metricDefinition.name],
+        { instanceId: this._instanceId, metricDate: { gt: this._cutOffDate } }])
         .pipe(take(1))
         .subscribe(data => {
-          if (data.length === 0) {
-            this.historyChart.dataTable = [['', {role: 'annotation'}], ['', '']];
-          } else {
-            this.historyChart.dataTable = [['Date', this._metricDefinition.unitLabel]];
-            data.sort(function(a, b) { 
-              return moment(a.metricDate).isBefore(b.metricDate) ? -1 : 1;
-            });
-            data.forEach((data) => {
-              this.historyChart.dataTable.push([moment(data.metricDate).format('MM/DD/YY'), Number(data.value)]);
-            });
-          }
+          let newDataTable = this._getDataTableFromMetricData(data);
 //          this.showSpinner = false;
-          this.historyChart.component.draw();
+          if (this.historyChart) {
+            this.historyChart.dataTable = newDataTable;
+            this.historyChart.component.draw();
+          } else {
+            this._initializeChart(this._metricDefinition, newDataTable);
+          }
         });
     }
   }
 
-  private _initializeChart(metric) {
+  private _initializeChart(metric, dataTable) {
+    let colorName = this._getColorName(metric.modelName);
     this.historyChart = {
       chartType: this.chartType,
-      dataTable: [['', {role: 'annotation'}], ['', '']],
+      dataTable: dataTable,
       options: {
+        colors: [colorName],
+        chartArea: { top: 20, left: 50 },
         title: metric.displayName,
         width: 600,
-        height: 400,
+        height: 300,
         legend: 'none',
-        vAxis: { title: metric.unitLabel }
+        vAxis: { title: '', format: (metric.unit === 'Currency') ? '$#' : undefined },
+        animation: {
+          duration: 1000,
+          easing: 'out',
+          startup: true
+        }
       }
     };
   }
+
+
+  private _getColorName(modelName: string) : string {
+    const colorScheme = [
+      { modelName: 'Client', colorName: 'gold' },
+      { modelName: 'Product', colorName: 'green' },
+      { modelName: 'DeliveryRoute', colorName: 'brown' }
+    ];
+    let colorInfo = colorScheme.find((info) => info.modelName === modelName);
+    if (colorInfo) {
+      return colorInfo.colorName;
+    }
+    return 'blue';    // default color
+  }
+
+  /**
+   * Make a chart data table using metric data.
+   * @params{Object[]} metricData
+   * @returns{Object[]} chart data table
+   */
+  private _getDataTableFromMetricData(metricData: any[]) :any[] {    
+    if (metricData.length === 0) {
+      return [['', {role: 'annotation'}], ['', '']];
+    }
+    let newDataTable = [];
+    newDataTable = [['Date', this._metricDefinition.unitLabel]];
+    metricData.sort(function(a, b) { 
+      return moment(a.metricDate).isBefore(b.metricDate) ? -1 : 1;
+    });
+    metricData.forEach((data) => {
+      newDataTable.push([moment(data.metricDate).format('MM/DD/YY'), Number(data.value)]);
+    });
+    
+    return newDataTable;
+  }  
 }
